b9b7994ca3740edaae0f92156b89ef6b3663cf73
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 04:00:57 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@
 def my_select(collection)
  holder = [] #for collection at the end
  counter = 0 #start at beginning of array
- 
+
  while counter < collection.length # condition to be met
     if yield(collection[counter]) == true #if the mumbojumbo results in true *which select gives
        holder.push(collection[counter]) #then push that element  into the new array
