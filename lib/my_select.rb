2fa59ff4c532280766c3ad34337249a999d7f1f7
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 03:39:17 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
+#.select = Returns an array containing all elements of an enum for which the given block returns a true value
+# You dictate what to return, and ruby will return it (within context of what you are operating on)
+
+
 def my_select(collection)
- # your code here!
+    holder = []
+    if collect.size == 0
+      return collection
+    elsif array.each do |value|
+      holder << yield(value)
+    end
+ hold
+   end
 end
